4e248a4fcad98184a9385babdbe6a2e4104dc8f8
root <vi-dragonfly@list.ru>
2023-06-09 16:02:18 +0000

    changes for work on the server

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def get_review(dvmn_token, tg_bot_token, tg_chat_id):
                 bot = Bot(token=tg_bot_token)
 
                 bot.send_message(
-                    chat_id=chat_id,
+                    chat_id=tg_chat_id,
                     text=message,
                     parse_mode='markdown')
                 logging.info('Бот отправил сообщение!')
@@ -50,14 +50,27 @@ def get_review(dvmn_token, tg_bot_token, tg_chat_id):
             elif status == 'timeout':
                 timestamp = reviews['timestamp_to_request']
 
-        except requests.exceptions.ReadTimeout:
-            logging.exception('Долгое ожидание ответа')
-            continue
-
+        except requests.exceptions.HTTPError:
+            logging.error('Ошибка при выполнении HTTP-запроса')
         except requests.exceptions.ConnectionError:
-            logging.exception('Ошибка соединения')
+            logging.error('Ошибка подключения')
             time.sleep(30)
             continue
+        except requests.exceptions.ReadTimeout:
+            pass
+
+
+class MyLogsHandler(logging.Handler):
+
+    def __init__(self, tg_log_bot_token, tg_chat_id):
+        super().__init__()
+        self.chat_id = tg_chat_id
+        self.bot = Bot(tg_log_bot_token)
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        tg_bot = self.bot
+        tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
 
 
 if __name__ == '__main__':
@@ -65,5 +78,11 @@ if __name__ == '__main__':
     dvmn_token = os.environ['DVMN_TOKEN']
     tg_bot_token = os.environ['TG_BOT_TOKEN']
     tg_chat_id = os.environ['TG_CHAT_ID']
+    tg_log_bot_token = os.environ['TG_LOG_BOT_TOKEN']
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    logger.addHandler(MyLogsHandler(tg_log_bot_token, tg_chat_id))
+    logging.info('Бот стартовал!')
 
     get_review(dvmn_token, tg_bot_token, tg_chat_id)
